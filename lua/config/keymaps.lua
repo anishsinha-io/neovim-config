b9d1f6e6b41e030fc472f06bf5f8feeeda8cccc3
Anish Sinha <anishsinha0128@gmail.com>
2023-07-24 16:28:58 -0400

    Initial commit

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,11 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+--
+local function map(mode, lhs, rhs, opts)
+  local options = { noremap = true, silent = true }
+  if opts then
+    options = vim.tbl_extend("force", options, opts)
+  end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
