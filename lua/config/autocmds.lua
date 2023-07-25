f68885597f50133fb3cb5ec60330621bc0154f16
Anish Sinha <anishsinha0128@gmail.com>
2023-07-25 14:59:34 -0400

    Add autocmd to format python files on save

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,17 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- vim.api.nvim_create_autocmd("FileType", {
+--   pattern = "python",
+--   callback = function()
+--     vim.schedule(function()
+--       print("called python auto format")
+--     end)
+--   end,
+-- })
+
+vim.api.nvim_create_autocmd("BufWinLeave", {
+  pattern = { "*.py" },
+  command = "!black .",
+})
